66c650533765fd8fead1a4424086031b181f04ce
minji0000 <bluurmi@gmail.com>
2025-04-01 21:23:36 +0900

    solving Q59413

--- programmers/lv4/Q59413.sql
+++ programmers/lv4/Q59413.sql
@@ -1,8 +1,10 @@
---2025-01-21
+--2025-04-01
+-- 오늘 내 생일
 SELECT DATE_FORMAT(DATETIME, '%H') AS HOUR
       ,COUNT(DATE_FORMAT(DATETIME, '%H')) AS COUNT
 FROM ANIMAL_OUTS
 GROUP BY DATE_FORMAT(DATETIME, '%H')
 ORDER BY 1
 
+-- https://school.programmers.co.kr/questions/84119
 -- 정답은 아님미다
