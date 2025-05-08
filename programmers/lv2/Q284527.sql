1b60c838c915c6dcdfb38c89f72f81ac01e5ea06
minji0000 <bluurmi@gmail.com>
2025-05-08 23:54:14 +0900

    solving 284527

--- programmers/lv2/Q284527.sql
+++ programmers/lv2/Q284527.sql
@@ -4,7 +4,9 @@ SELECT  C.SCORE
      ,B.EMP_NAME
      ,B.POSITION
      ,B.EMAIL
+     ,C.HALF_YEAR
 FROM HR_DEPARTMENT A INNER JOIN HR_EMPLOYEES B
-ON A.DEPT_ID = B.DEPT_ID
-INNER JOIN HR_GRADE C
-ON B.EMP_NO = C.EMP_NO
+                                ON A.DEPT_ID = B.DEPT_ID
+                     INNER JOIN HR_GRADE C
+                                ON B.EMP_NO = C.EMP_NO
+WHERE C.HALF_YEAR IN (1, 2)
