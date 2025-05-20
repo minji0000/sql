e546c2e01adf84bb3d3bb22fa883bf4a4a875d53
minji0000 <bluurmi@gmail.com>
2025-05-20 23:08:02 +0900

    solving 298519

--- programmers/lv3/Q298519.sql
+++ programmers/lv3/Q298519.sql
@@ -1,3 +1,12 @@
+--2025-05-20
+SELECT  COUNT(FISH_TYPE) AS FISH_COUNT
+     , REPLACE(DATE_FORMAT(TIME, '%m'), '0', '') AS MONTH
+FROM FISH_INFO
+GROUP BY REPLACE(DATE_FORMAT(TIME, '%m'), '0', '')
+ORDER BY CAST(REPLACE(DATE_FORMAT(TIME, '%m'), '0', ''), UNSIGNED INTEGER)
+-- 값은 제대로 나오는데 정렬이 쪼오끔..
+
+
 --2025-05-16
 select COUNT(fish_type), DATE_FORMAT(time, 'Y%m%')
 from fish_info
