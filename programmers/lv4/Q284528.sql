dda4c252d4f48f94407ee66a897c58dac13917c5
minji0000 <bluurmi@gmail.com>
2025-06-11 01:39:58 +0900

    solving group by

--- programmers/lv4/Q284528.sql
+++ programmers/lv4/Q284528.sql
@@ -1,4 +1,35 @@
--- 2025-06-10
+-- 2025-06-11
+SELECT *
+FROM (
+
+         SELECT   B.EMP_NO
+              ,B.EMP_NAME
+              ,C.HALF_YEAR
+              ,C.SCORE
+         FROM HR_DEPARTMENT A INNER JOIN HR_EMPLOYEES B
+                                         ON A.DEPT_ID = B.DEPT_ID
+                              INNER JOIN HR_GRADE C
+                                         ON B.EMP_NO = C.EMP_NO
+         WHERE C.HALF_YEAR = '1'
+
+         UNION
+
+         SELECT   B.EMP_NO
+              ,B.EMP_NAME
+              ,C.HALF_YEAR
+              ,C.SCORE
+         FROM HR_DEPARTMENT A INNER JOIN HR_EMPLOYEES B
+                                         ON A.DEPT_ID = B.DEPT_ID
+                              INNER JOIN HR_GRADE C
+                                         ON B.EMP_NO = C.EMP_NO
+         WHERE C.HALF_YEAR = '2'
+     )
+-- Every derived table must have its own alias
+
+
+
+
+
 SELECT   B.EMP_NO
         ,B.EMP_NAME
         ,C.HALF_YEAR
